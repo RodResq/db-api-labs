26e193197bbee57b3460f14144caa5111ed372a0
RodResq <rodrigoresque@yahoo.com.br>
2021-12-23 19:10:26 -0300

    inserindo parametros na query

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
-db = MySQLdb.connect(user="root", passwd="123", db="treinaweb_clientes", host="localhost", port=3306, autocommit=False)
+db = MySQLdb.connect(user="root", passwd="123", db="treinaweb_clientes", host="localhost", port=3306, autocommit=True)
 print("Conexao realizada com sucesso em ")
 
 cursor = db.cursor()
@@ -8,15 +8,15 @@ cursor.execute("SELECT * FROM cliente")
 print(cursor.fetchmany(1))
 
 # Transacao
-try:
-    db.begin()
-    cursor.execute("INSERT INTO cliente (nome, idade) VALUES ('Pedro', 25)")
-    cursor.execute("INSERT INTO cliente (nome, idade) VALUES ('Joana', 25)")
-    db.commit()
-except:
-    db.rollback()
-print(cursor.execute("SELECT * FROM cliente"))
-print(cursor.fetchall())
+# try:
+#     db.begin()
+#     cursor.execute("INSERT INTO cliente (nome, idade) VALUES ('Pedro', 25)")
+#     cursor.execute("INSERT INTO cliente (nome, idade) VALUES ('Joana', 25)")
+#     db.commit()
+# except:
+#     db.rollback()
+# print(cursor.execute("SELECT * FROM cliente"))
+# print(cursor.fetchall())
 
 # cursor.execute("SELECT * FROM cliente")
 # print(cursor.fetchall())
@@ -29,5 +29,12 @@ print(cursor.fetchall())
 # cursor.execute("DELETE FROM cliente WHERE idCliente=9")
 # cursor.execute("SELECT * FROM cliente")
 # print(cursor.fetchall())
+
+# Passando parametro para a consulta
+nome = "Josefina"
+idade = 80
+cursor.execute("UPDATE cliente SET nome=%(nome)s, idade=%(idade)s WHERE idCLiente=2", ({'nome': nome, 'idade': idade}))
+cursor.execute("SELECT * FROM cliente")
+print(cursor.fetchall())
 
 db.close()
