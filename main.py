86c40c6851fae71042d95d80f92c1f9f747c66c1
RodResq <rodrigoresque@yahoo.com.br>
2021-12-23 16:47:58 -0300

    Cursores e metodos execute e fetch...

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import MySQLdb
 
 db = MySQLdb.connect(user="root", passwd="123", db="treinaweb_clientes", host="localhost", port=3306)
+cursor = db.cursor()
+cursor.execute("SELECT * FROM cliente")
+print(cursor.fetchmany(1))
 print("Conexao realizada com sucesso em ")
 db.close()
 
