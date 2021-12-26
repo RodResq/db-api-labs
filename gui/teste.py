855a52240a004547c153ca91dd3987074da2192b
RodResq <rodrigoresque@yahoo.com.br>
2021-12-25 22:25:34 -0300

    teste rativos

---
+++ gui/teste.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.properties import StringProperty, NumericProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+
+class Principal(BoxLayout):
+    texto_principal = StringProperty('Eu sou uma label')
+    tamanho_texto_principal = NumericProperty(30)
+
+    def teste(self):
+        self.texto_principal = 'Fui Clicado'
+        self.tamanho_texto_principal += 20
+
+
+class Secundaria(Widget):
+    pass
+
+
+class Teste(App):
+    def build(self):
+        return Principal()
+
+
+Teste().run()
